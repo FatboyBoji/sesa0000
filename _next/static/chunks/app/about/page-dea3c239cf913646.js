dba94ddc9f9831bcac741de2b81a8f331125ad6d
FatboyBoji <FatboyBoji@users.noreply.github.com>
2024-12-20 22:11:37 +0000

    deploy: 0acd7fa4b6b80511325a3a9d1dc1d9b0a13ee5f8

---
+++ _next/static/chunks/app/about/page-dea3c239cf913646.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301],{5468:(e,s,n)=>{Promise.resolve().then(n.bind(n,9966)),Promise.resolve().then(n.bind(n,2554))}},e=>{var s=s=>e(e.s=s);e.O(0,[988,838,130,215,744],()=>s(5468)),_N_E=e.O()}]);
